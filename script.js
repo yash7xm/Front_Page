13c039e53600c5e94d5ac1daf691ae015aab338a
Yash Poonia <93772907+yash7xm@users.noreply.github.com>
2023-05-11 01:05:10 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,46 @@ let scrollDownStop = false;
 let scrollTopStop = false;
 let activeIndex = 0;
 const slides = document.getElementsByTagName("article");
+const mouseCaret = document.querySelector('.mouseCaret');
+const navLinks = document.querySelectorAll('nav a');
+let rightBtn = document.querySelectorAll('.move-btns .right-btn');
+let leftBtn = document.querySelectorAll('.move-btns .left-btn');
+
+
+window.addEventListener('mousemove', (e) => {
+    mouseCaret.style.top = e.pageY + 'px';
+    mouseCaret.style.left = e.pageX + 'px';
+})
+
+rightBtn.forEach(btn => {
+    btn.addEventListener('mouseover', () => {
+        mouseCaret.classList.add('caret-grow');
+    })
+    btn.addEventListener('mouseleave', () => {
+        mouseCaret.classList.remove('caret-grow');
+    })
+})
+
+leftBtn.forEach(btn => {
+    btn.addEventListener('mouseover', () => {
+        mouseCaret.classList.add('caret-grow');
+    })
+    btn.addEventListener('mouseleave', () => {
+        mouseCaret.classList.remove('caret-grow');
+    })
+})
+
+navLinks.forEach(link => {
+    link.addEventListener('mouseover', () => {
+        console.log('inside nav link');
+        mouseCaret.classList.add('caret-grow');
+        link.classList.add('hovered-link');
+    })
+    link.addEventListener('mouseleave', () => {
+        mouseCaret.classList.remove('caret-grow');
+        link.classList.remove('hovered-link');
+    })
+})
 
 function handleMouseEnter() {
     console.log("Mouse entered!");
@@ -10,6 +50,7 @@ function handleMouseEnter() {
     const headingText = document.querySelector(`[data-index="${activeIndex}"] .heading .heading-text`);
     const rotatingIcon = document.querySelector(`[data-index="${activeIndex}"] .heading .rotating-icon img`);
     scrollTopStop = true;
+    mouseCaret.classList.add('caret-grow-heading');
     scrollToBottom(headingText.scrollTop, headingText.scrollHeight, headingText);
     rotatingIcon.style.transform = "rotate(90deg)";
     rotatingIcon.style.transition = "transform 0.3s ease-out";
@@ -21,6 +62,7 @@ function handleMouseEnter() {
     const headingText = document.querySelector(`[data-index="${activeIndex}"] .heading .heading-text`);
     const rotatingIcon = document.querySelector(`[data-index="${activeIndex}"] .heading .rotating-icon img`);
     scrollDownStop = true;
+    mouseCaret.classList.remove('caret-grow-heading');
     scrollToTop(headingText.scrollTop, 0, headingText);
     rotatingIcon.style.transform = "rotate(0deg)";
     rotatingIcon.style.transition = "transform 0.3s ease-out";
@@ -63,11 +105,12 @@ function handleRightBtn(){
     currentSlide.dataset.status = "before";
     
     nextSlide.dataset.status = "becoming-active-from-after";
-    
+
     setTimeout(() => {
       nextSlide.dataset.status = "active";
       activeIndex = nextIndex;
     });
+    
 }
 
 function handleLeftBtn(){
